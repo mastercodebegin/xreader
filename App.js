af6b76258c43a0c5dad1ef9275204715119584de
Firoz <firoz.k@protonshub.com>
2022-08-25 15:59:42 +0530

    pdf-view

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import React from 'react'
 import Navigations from './src/navigation/Navigations'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import Store from './src/context/store/Store'
 import Stepper from './src/helper/util/Stepper'
 import DropDown from './src/component/DropDown'
 import Rating from './src/helper/util/Rating'
 import Chat from './src/helper/util/Chat'
-
-
-export default App=()=>{
-  return(
-  // <Dashboard/>
+import Pdf from 'react-native-pdf';
+const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
+export default App = () => {
+  return (
+    // <Dashboard/>
     <Provider store={Store}>
-          {/* <Navigations/> */}
-          <Chat/>
-          </Provider>
+      {/* <Navigations/> */}
+      <View style={styles.container}>
+        <Pdf
+        trustAllCerts={false}
+          source={source}
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`Current page: ${page}`);
+          }}
+          onError={(error) => {
+            console.log(error);
+          }}
+          onPressLink={(uri) => {
+            console.log(`Link pressed: ${uri}`);
+          }}
+          style={styles.pdf} />
+      </View>
+    </Provider>
 
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+});
